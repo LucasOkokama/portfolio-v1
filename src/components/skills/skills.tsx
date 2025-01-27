9c987ecc65edeb35536c58f785ea8dfb2eb32a2b
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2025-01-27 01:21:54 -0300

    refactor(skills): import component instead of writing code manually

--- src/components/skills/skills.tsx
+++ src/components/skills/skills.tsx
@@ -1,10 +1,7 @@
 "use client";
 
-import { Geist } from "next/font/google";
+import GlitchText from "../ui/glitch-text/glitch-text";
 import SkillsCard from "./skills-card";
-import styles from "./css/skills.module.css";
-
-const geistSans = Geist({ subsets: ["latin"] });
 
 export default function Skills() {
   return (
@@ -13,12 +10,7 @@ export default function Skills() {
         {/* Divider 1 */}
         <div className="h-[2px] w-[25%] rounded-full bg-neutral-200 dark:bg-neutral-800/60"></div>
         {/* Title */}
-        <h2
-          className={`${geistSans.className} ${styles.title} relative select-none text-5xl font-extrabold text-neutral-800 before:bg-white/[0.97] before:bg-texture before:bg-blend-lighten after:bg-white/[0.97] after:bg-texture after:bg-blend-lighten dark:text-neutral-100 dark:before:bg-neutral-950/[0.97] dark:before:bg-blend-darken dark:after:bg-neutral-950/[0.97] dark:after:bg-blend-darken`}
-          data-title="Skills"
-        >
-          Skills
-        </h2>
+        <GlitchText text="Skills" />
         {/* Divider 2 */}
         <div className="h-[2px] w-[25%] rounded-full bg-neutral-200 dark:bg-neutral-800/60"></div>
       </div>

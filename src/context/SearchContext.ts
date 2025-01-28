f76b28919edac3755ccc02e48c308099add95b40
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2025-01-28 03:03:17 -0300

    feat(searchContext): create search context

---
+++ src/context/SearchContext.ts
@@ -0,0 +1,18 @@
+import { createContext, useContext } from "react";
+
+export interface SearchContextType {
+  searchValue: string,
+  setSearchValue: React.Dispatch<React.SetStateAction<string>>;
+}
+
+// Create the context
+export const SearchContext = createContext<SearchContextType | undefined>(undefined);
+
+// Create the function to get the context value
+export const useSearchContext = (): SearchContextType => {
+  const context = useContext(SearchContext);
+  if (context === undefined) {
+    throw new Error("Context not found!");
+  }
+  return context;
+}

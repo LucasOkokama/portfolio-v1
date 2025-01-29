fce4ed1f7eda782ae0d59387b3a3d1a17ba61284
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2025-01-29 19:47:03 -0300

    feat(layout/pages): add new component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,29 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark relative scroll-smooth">
+    <html lang="en" className={`relative scroll-smooth`} suppressHydrationWarning>
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function() {
+                try {
+                  const userTheme = localStorage.getItem("theme") || "system";
+
+                  const systemPrefersDark = window.matchMedia(
+                    "(prefers-color-scheme: dark)",
+                  ).matches;
+
+                  document.documentElement.classList.toggle(
+                    "dark",
+                    userTheme === "dark" || (userTheme === "system" && systemPrefersDark),
+                  );
+                } catch (e) {}
+              })();
+            `,
+          }}
+        />
+      </head>
       <body
         className={`${rethinkSans.className} bg-white text-black transition-colors duration-500 before:absolute before:z-[-1] before:h-full before:w-full before:bg-texture before:opacity-[3%] dark:bg-neutral-950 dark:text-white`}
       >
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,8 @@ import Skills from "@/components/skills/skills";
 import { useState } from "react";
 import { SearchContext } from "@/context/SearchContext";
 import Statistics from "@/components/statistics/statistics";
+import BlackHole from "@/components/ui/black-hole";
+import { DarkModeContext } from "@/context/DarkModeContext";
 
 const menuFadeIn = {
   hidden: { y: -200 },
@@ -26,51 +28,61 @@ export default function Home() {
   const [searchValue, setSearchValue] = useState<string>("");
   const [showMoreCount, setShowMoreCount] = useState<number>(3);
 
+  const [darkModeEnabled, setDarkModeEnabled] = useState(false);
+
   return (
-    <div className="flex h-full w-full justify-center px-5">
-      <div className="h-full w-full max-w-5xl">
-        <div className="flex w-full flex-col">
-          <motion.div
-            variants={menuFadeIn}
-            initial="hidden"
-            animate="visible"
-            className="sticky top-5 z-[5]"
-          >
-            <Menu />
-          </motion.div>
-          <div className="flex justify-center">
-            <div className="flex h-full w-full max-w-4xl flex-col items-center justify-center gap-[50vh] pt-24 md:pt-0">
-              <div
-                id="about"
-                className="flex min-h-screen w-full scroll-mt-44 items-center"
+    <div className="relative flex h-full w-full flex-col">
+      { darkModeEnabled && <BlackHole />}
+      <div className="flex h-full w-full justify-center px-5">
+        <div className="h-full w-full max-w-5xl">
+          <div className="flex w-full flex-col">
+            <DarkModeContext.Provider
+              value={{ darkModeEnabled, setDarkModeEnabled }}
+            >
+              <motion.div
+                variants={menuFadeIn}
+                initial="hidden"
+                animate="visible"
+                className="sticky top-5 z-[5]"
               >
-                <About />
-              </div>
+                <Menu />
+              </motion.div>
+            </DarkModeContext.Provider>
 
-              <div id="skills" className="w-full scroll-mt-44">
-                <Skills />
-              </div>
-
-              <div id="projects" className="w-full scroll-mt-44">
-                <SearchContext.Provider
-                  value={{
-                    searchValue,
-                    setSearchValue,
-                    showMoreCount,
-                    setShowMoreCount,
-                  }}
+            <div className="flex justify-center">
+              <div className="flex h-full w-full max-w-4xl flex-col items-center justify-center gap-[50vh] pt-[12vh] md:pt-0">
+                <div
+                  id="about"
+                  className="flex min-h-screen w-full scroll-mt-44 items-center"
                 >
-                  <Projects />
-                </SearchContext.Provider>
-              </div>
+                  <About />
+                </div>
 
-              <div id="statistics" className="w-full">
-                <Statistics />
+                <div id="skills" className="w-full scroll-mt-44">
+                  <Skills />
+                </div>
+
+                <div id="projects" className="w-full scroll-mt-44">
+                  <SearchContext.Provider
+                    value={{
+                      searchValue,
+                      setSearchValue,
+                      showMoreCount,
+                      setShowMoreCount,
+                    }}
+                  >
+                    <Projects />
+                  </SearchContext.Provider>
+                </div>
+
+                <div id="statistics" className="w-full">
+                  <Statistics />
+                </div>
               </div>
             </div>
-          </div>
-          <div className="mt-40 flex justify-center">
-            <Footer />
+            <div className="mt-40 flex justify-center">
+              <Footer />
+            </div>
           </div>
         </div>
       </div>

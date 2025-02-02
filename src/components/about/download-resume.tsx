f909c97f967c26c4752f4c8396b39a4a87ffdff4
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2025-02-02 17:44:02 -0300

    feat(resume): add resume pdf file

--- src/components/about/download-resume.tsx
+++ src/components/about/download-resume.tsx
@@ -1,38 +1,50 @@
 export default function DownloadResume() {
   return (
-    <div className="flex h-[40px] w-[45%] min-w-72 md:w-full rounded-lg bg-gradient-to-r from-[#E447A0] to-[#853BE6] text-white">
-      <button className="flex w-full items-center justify-center rounded-l-lg">
-        <span className="ml-5 flex gap-2">
-          Baixar Currículo
-          {/* Download icon */}
+    <div className="flex h-[40px] w-[45%] min-w-72 rounded-lg bg-gradient-to-r from-[#E447A0] to-[#853BE6] text-white md:w-full">
+      <a
+        href="/Lucas_Okokama_Curriculo.pdf"
+        download="Lucas_Okokama_Curriculo"
+        className="flex w-full items-center justify-center rounded-l-lg"
+      >
+        <button>
+          <span className="ml-5 flex gap-2">
+            Baixar Currículo
+            {/* Download icon */}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 -960 960 960"
+              width="24px"
+              fill="currentColor"
+            >
+              <path d="M480-320 280-520l56-58 104 104v-326h80v326l104-104 56 58-200 200ZM240-160q-33 0-56.5-23.5T160-240v-120h80v120h480v-120h80v120q0 33-23.5 56.5T720-160H240Z" />
+            </svg>
+          </span>
+        </button>
+      </a>
+      <a
+        href="/Lucas_Okokama_Curriculo.pdf"
+        target="_blank"
+        className="flex w-[20%] items-center justify-center rounded-r-lg border-l-2 border-white duration-200 dark:border-neutral-950"
+      >
+        <button>
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            height="24px"
-            viewBox="0 -960 960 960"
-            width="24px"
-            fill="currentColor"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           >
-            <path d="M480-320 280-520l56-58 104 104v-326h80v326l104-104 56 58-200 200ZM240-160q-33 0-56.5-23.5T160-240v-120h80v120h480v-120h80v120q0 33-23.5 56.5T720-160H240Z" />
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
+            <path d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6" />
           </svg>
-        </span>
-      </button>
-      <button className="flex w-[20%] items-center justify-center rounded-r-lg border-l-2 border-white duration-200 dark:border-neutral-950">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        >
-          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-          <path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
-          <path d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6" />
-        </svg>
-      </button>
+        </button>
+      </a>
     </div>
   );
 }
